3204a34be36e8b25ad1a3c0616650ea641dacd2a
Beyza Ökten <beyzaokten19@gmail.com>
2023-05-30 00:32:18 +0300

    Etkinlik açıklaması ve başlığı için sayfa oluşturuldu.

---
+++ app/src/main/java/com/example/takvimuygulamasi/AddEventActivity.java
@@ -0,0 +1,54 @@
+package com.example.takvimuygulamasi;
+
+
+import static com.example.takvimuygulamasi.R.id.date_textView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+
+public class AddEventActivity extends AppCompatActivity {
+
+    private EditText eventEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_event);
+
+
+
+        //Tarihi getir
+        Intent intent = getIntent();
+        String date = intent.getStringExtra("selectedDate");
+
+        setTitle(date);
+
+
+        //Kaydetme butonu
+        Button saveButton = findViewById(R.id.save_button);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String event = eventEditText.getText().toString();
+
+                //Database girişi yapılmalı.
+
+                // Geri dönüş
+                Intent intent = new Intent(AddEventActivity.this, EventActivity.class);
+                intent.putExtra("selectedDate", date);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
